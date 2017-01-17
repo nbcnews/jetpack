a526048108f0e1c07d097e6a8178c600e84b79bc
Todd Wallar <todd.wallar@gmail.com>
2017-01-17 12:53:09 -0800

    Got working as `npm run` script in local project.

--- bundle.js
+++ bundle.js
@@ -1,82 +1,90 @@
 const path = require('path');
-const webpack = require("webpack");
+const webpack = require('webpack');
+const info = require('./lib/info');
 
 var options = {
-  "site": 'today',
-  "minify": true
+    "site": 'today',
+    "minify": true
 };
 
 var plugins = [];
 var isDev = false;
 
-var workingDir = __dirname;
+var workingDir = process.env.PWD || __dirname;
 
 //if we are being run as an npm module, then use the parent path instead.
 if (__dirname.indexOf('node_modules') !== -1) {
-  workingDir = (__dirname.split('/node_modules'))[0];
+    workingDir = (__dirname.split('/node_modules'))[0];
+}
+
+//info.log(process.env);
+
+var args = JSON.parse(process.env.npm_config_argv);
+
+if (typeof args.remain !== 'undefined') {
+    options.site = args.remain[0]; //get site name when running as npm script
 }
 
 process.argv.forEach(function (val, index, array) {
-  const splitted = val.split('/');
-  if ((splitted[splitted.length - 1] === 'bundle') && (typeof process.argv[index + 1] !== "undefined")) {
-    options.site = process.argv[index + 1];
-  }
-
-  if (val === '-dev') {
-    isDev = true;
-    options.minify = false;
-  }
-
+    if (val === '-dev') {
+        isDev = true;
+        options.minify = false;
+        return;
+    }
 });
 
+
+info.log('packaging ' + options.site);
+
+
 if (options.minify) {
-  plugins.push(new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      mangle: {
-        //props: true
-        //toplevel: true
-      }
-    })
-  );
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            mangle: {
+                //props: true
+                //toplevel: true
+            }
+        })
+    );
 }
 
 const filename =  options.site + '_bundle' + (options.minify?'_min':'') + '.js';
 
 const wpConfig = {
-  context: __dirname,
-  entry: "./sites/" + options.site + ".js",
-  output: {
-    "path": __dirname + '/dist/' + options.site + '/',
-    "filename":  filename,
-    "publicPath": '/' + options.site + '/'
-  },
-  resolve: {
-    modules: [
-      path.resolve(__dirname)
-    ]
-  },
-  module: {
-    loaders: [{
-      test: /\.css$/,
-      loader: "style-loader!css-loader"
-    },{
-      test: /\.html/,
-      loader: "html-loader"
-    }, {
-      test: /\.(png|jpg|jpeg|gif|woff)$/,
-      loader: "file-loader?name=[name].[ext]"
-    }]
-  },
-  "plugins": plugins
+    context: workingDir,
+    entry: "./sites/" + options.site + ".js",
+    output: {
+        "path": workingDir + '/dist/' + options.site + '/',
+        "filename":  filename,
+        "publicPath": '/' + options.site + '/'
+    },
+    resolve: {
+        modules: [
+            path.resolve(workingDir)
+        ]
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        },{
+            test: /\.html/,
+            loader: "html-loader"
+        }, {
+            test: /\.(png|jpg|jpeg|gif|woff)$/,
+            loader: "file-loader?name=[name].[ext]"
+        }]
+    },
+    "plugins": plugins
 }
 
 
 var compiler = webpack(wpConfig);
 
 compiler.run(function(err, stats) {
-  if (isDev) {
-    console.log(stats);
-  }
+    if (isDev) {
+        console.log(stats);
+    }
 });
 
 
@@ -85,7 +93,7 @@ compiler.run(function(err, stats) {
 /**
  We could also do
 
-compiler.watch({ // watch options:
+ compiler.watch({ // watch options:
   aggregateTimeout: 300, // wait so long for more changes
   poll: true // use polling instead of native watchers
   // pass a number to set the polling interval
