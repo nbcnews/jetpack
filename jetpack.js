8421e4fa868480bd6d8f0d7853fc9672d8c129fd
Todd Wallar <todd.wallar@gmail.com>
2017-01-18 17:38:52 -0800

    moved runWhenAvailable to runner/require

--- jetpack.js
+++ jetpack.js
@@ -4,9 +4,13 @@
 import * as utils from 'lib/basicUtils';
 import * as info from 'lib/info';
 import * as mgr from 'lib/optionsManager';
+import * as requireRunner from 'lib/runner/require';
 
 exports = {
   "utils" : utils,
   "info" : info,
-  "mgr" : mgr
+  "mgr" : mgr,
+  "runner" : {
+    "require" : requireRunner
+  }
 };
